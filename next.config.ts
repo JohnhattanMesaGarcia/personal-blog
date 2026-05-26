f51f579394fa7883b57382d437f7988f8051b8fa
Johnhattan Alexander Mesa García <jmesag08@gmail.com>
2026-05-25 20:36:31 -0500

    Next

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
